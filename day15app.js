3843c2822f2c6dccbc8cce235c38a6b85d2795c7
shivraj7974 <105036727+shivraj7974@users.noreply.github.com>
2024-06-26 14:35:37 +0530

    Add files via upload

---
+++ day15app.js
@@ -0,0 +1,15 @@
+console.log("hello bro");
+let a=29;
+let b=54;
+console.log("sum is",a+b);
+
+
+let age=68;
+if(age>=18)
+{
+    console.log("you can vote");
+}
+console.log("after");  
+
+let firstname = prompt("enter your name");
+console.log(firstname);
